b10f3225ae63fe3d64a8c0e536e29907176ef20d
maxlethal <67437106+maxlethal@users.noreply.github.com>
2023-06-08 18:08:51 +0300

    1st commit - cool completed + requirments

--- parse.py
+++ parse.py
@@ -1,38 +1,73 @@
 from db import *
 from dicts import black_labels
-# from pydantic import BaseModel
 
 import random
-import datetime as dt
-import time
 from loguru import logger
 from dateutil import parser
 import requests
 from bs4 import BeautifulSoup
 
-from apscheduler.schedulers.background import BlockingScheduler
+
+class AgenciesID(DataBaseMixin):
+	def __init__(self):
+		self.ids_dict = dict()
+
+	@staticmethod
+	def __get_agencies_ids() -> dict:
+		"""Отдаёт словарь всех существующих id новостей для каждого агенства"""
+		"""{'agency': (id1, id2,...)}"""
+		all_agencies_ids = {}
+		news_ids = Query.get_all_ids(smi, 'news')
+		final_ids = Query.get_all_ids(smi, 'final')
+		asmi_ids = Query.get_all_ids(asmi, 'news')
+		total_ids = [*news_ids, *final_ids, *asmi_ids]
+		all_active_agencies_query = "SELECT telegram FROM agencies WHERE is_parsing is True"
+		all_active_agencies = DataBaseMixin.get(all_active_agencies_query, asmi)
+
+		for agency in all_active_agencies:
+			articles_ids = tuple(
+				el['id'] for el in total_ids if el['agency'] == agency['telegram'])
+			all_agencies_ids[agency['telegram']] = articles_ids
+
+		return all_agencies_ids
+
+	@property
+	def get_ids(self):
+		return self.ids_dict if self.ids_dict else logger.error(f'Словарь id пока не сформирован')
+
+	@property
+	def set_ids(self):
+		self.ids_dict = self.__get_agencies_ids() if not self.ids_dict else logger.error('Словарь id уже в наличие')
+
+	@property
+	def del_ids(self):
+		self.ids_dict.clear()
+		logger.info('Словарь id новостей успешно очищен')
+
+	def get_agency(self, agency):
+		return self.ids_dict[agency]
 
 
 class Parser(Query):
 	"""Класс парсера: собирает новости агентства, сохраняет словарь новостей и позволяет работать с ним """
 
-	def __init__(self, channel: str, last_id: int):
+	def __init__(self, channel: str, ids: tuple):
 		self.channel = channel
-		self.last_id = last_id
+		self.ids = ids
 		self.news = []
-		self.exist_urls = Query.get_all_articles_dict(smi)
 
 	def __len__(self):
 		return len(self.news)
 
 	def __grab_news(self):
 		"""Создаёт словарь последних новостей СМИ и возвращает его"""
-		my_news = []
-		agency = self.channel.split('/')[-1]
+		my_news: list[dict] = []
+		agency = self.channel
+		agency_url = 'https://t.me/s/' + agency
 		user_agents = open('proxy/user-agents.txt').read().splitlines()
 		random_user_agent = random.choice(user_agents)
 		headers = {'User-Agent': random_user_agent}
-		answer = requests.get(self.channel, headers=headers)
+		answer = requests.get(agency_url, headers=headers)
 		if answer and answer.status_code != 204:
 			try:
 				soup = BeautifulSoup(answer.text, features="html.parser")
@@ -41,7 +76,7 @@ class Parser(Query):
 					dirty_news = news_content.find(attrs={'class': 'tgme_widget_message_text js-message_text'})
 					news_id = int(news_content.find(attrs={'class': 'tgme_widget_message_date'}).get('href').split('/')[
 						              -1])
-					if dirty_news and (news_id > self.last_id and news_id not in self.exist_urls[agency]):
+					if dirty_news and (news_id not in self.ids):
 						news = Parser.clean_news(dirty_news.text, agency)
 						url = news_content.find(attrs={'class': 'tgme_widget_message_date'}).get('href')
 						links = dirty_news.find('a').get('href').split('?utm')[0] if dirty_news.find(
@@ -78,34 +113,3 @@ class Parser(Query):
 		for label in total_label:
 			news = news.replace(label, ' ')
 		return news.strip()
-
-
-def start_parsing():
-	last_news_id = Query.get_last_news_id()
-	total_news = 0
-	start_time = dt.datetime.now()
-	logger.info(f'Начинается обработка новостей от {start_time}:\n')
-	if DataBaseMixin.is_not_empty('news'):
-		logger.error(
-			f'Сбор новостей не может быть проведён, так как временная база данных smi по какой-то не пуста')
-		logger.info('Очистите базу данных вручную и запустите снова')
-	for full_channel_link, last_id in last_news_id.items():
-		channel_name = full_channel_link.split("/")[-1]
-		channel = Parser(full_channel_link, last_id)
-		channel.set_news
-		if len(channel):
-			DataBaseMixin.record(smi, 'news', channel.get_news)
-			logger.info(f'{channel_name}: собрано {len(channel)} новостей')
-		total_news += len(channel)
-		channel.del_news
-		time.sleep(random.randint(3, 5))
-	result_time = dt.datetime.now() - start_time
-	logger.info(
-		f'\nСбор завершен успешно.\nПолучено {total_news} новостей за {round(result_time.seconds / 60, 2)} минут')
-
-
-if __name__ == '__main__':
-	# scheduler = BlockingScheduler()
-	# scheduler.add_job(start_parsing, 'interval', hours=1, next_run_time=dt.datetime(2023, 5, 26, 21, 55, 0))
-	# scheduler.start()
-	start_parsing()
